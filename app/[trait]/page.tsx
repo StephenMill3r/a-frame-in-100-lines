6897fbc368cb06b1096319813fed309902f684b8
StephenMill3r <stephen@cryptodadsnft.com>
2024-02-17 19:02:22 -0500

    update

--- app/[trait]/page.tsx
+++ app/[trait]/page.tsx
@@ -1,33 +1,11 @@
-// pages/frame/[trait].tsx
-import { GetServerSideProps, Metadata, ResolvingMetadata } from 'next';
+
+import { Metadata, ResolvingMetadata } from 'next';
+
+
 import { getFrameMetadata } from '@coinbase/onchainkit';
 
 
 import { NEXT_PUBLIC_URL } from '../config';
-
-
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { trait } = context.params;
-
-  // Construct your metadata here
-//   const metadata = {
-//     title: 'CryptoDad Trait Viewer Frame',
-//     description: 'LFG',
-//     openGraph: {
-//       title: 'this is my title',
-//       description: 'LFG',
-//       images: [`https://cryptodadsnft.nyc3.cdn.digitaloceanspaces.com/steak-shop-images/${trait}.png`],
-//     },
-//     trait: trait,
-//   };
-   
-  return {
-    props: { trait }, // Pass metadata as props to your page
-  };
-};
-
-
 
  
 type Props = {
@@ -48,7 +26,7 @@ export async function generateMetadata(
       },
       {
         action: 'link',
-        label: 'Buy 2Clickz Trait for 5000 $steak',
+        label: `Buy ${trait} Trait`,
         target: 'https://steak.cryptodadsnft.com/shop?filter=CUSTOMIZE-DAD',
       },
     ],
@@ -90,46 +68,3 @@ export default function Page({ params, searchParams }: Props) {
       </>
     );
   }
-  
-
-// function Page({ metadata }) {
-//   if (!metadata) return <div>Loading...</div>;
-//   return (
-//     <>
-//       <Head>
-//         <title>{metadata.title}</title>
-//         <meta name="description" content={metadata.description} />
-//         {metadata.openGraph.images.map((image, index) => (
-//           <meta key={index} property="og:image" content={image} />
-//         ))}
-
-//         <meta name="fc:frame" content="vNext" />
-//         <meta name="fc:frame:button:1" content="Select Dad" />
-//         <meta name="fc:frame:button:2" content={"Buy "+metadata.trait+" Trait"} />
-//         <meta name="fc:frame:button:2:action" content="link" />
-//         <meta name="fc:frame:button:2:target" content="https://steak.cryptodadsnft.com/shop?filter=CUSTOMIZE-DAD" />
-//         {'<meta name="fc:frame:image" content="'+metadata.openGraph.images[0]+'.png />'}
-//         <meta name="fc:frame:image:aspect_ratio" content="1:1" />
-//           <meta name="fc:frame:input:text" content="Enter your CryptoDad #" />
-//           {'<meta name="fc:frame:post_url" content="'+NEXT_PUBLIC_URL+'/api/dadframe?trait='+metadata.trait+'" />'}
-//       </Head>
-//       {/* Page content */}
-//       <h1>{metadata.title}</h1>
-//     </>
-//   );
-// };
-
-
-// const handler = async (req, res) => {
-//     const { method } = req
-//     switch (method) {
-//       case 'GET':
-//         await nfts(req, res)
-//         break
-//       default:
-//         res.setHeader('Allow', ['GET', 'OPTIONS'])
-//         res.status(405).end(`Method ${method} Not Allowed`)
-//     }
-//   }
-  
-//   export default cors(handler)
